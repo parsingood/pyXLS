39a721f33617a61d28796c3f35568bb6e2d02cd7
Ivan Mihaylov <ivan.mihaylov.bg@gmail.com>
2025-10-10 11:17:53 +0300

    gh

---
+++ gh.bat
@@ -0,0 +1,13 @@
+@echo off
+cd /d C:\ivanm\pyXLS
+
+:: Пита потребителя за описание на промените
+set /p msg=Въведи описание на промените: 
+
+git add .
+git commit -m "%msg%"
+git push
+
+echo.
+echo ✅ Промените са качени успешно в GitHub!
+pause
